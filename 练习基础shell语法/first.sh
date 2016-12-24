99417a3e24e0413d6ebb6cd9c4162e7ceebd4452
faraco <skelic3@gmail.com>
2016-12-24 23:23:33 +0800

    Update first.sh

--- 练习基础shell语法/first.sh
+++ 练习基础shell语法/first.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 # This script displays the date and who's logged on
 
-echo -n " this is current time: "
-date
+printf " this is current time: $(date)\n"
 
-echo "Let's see who's looged into the system"
-who
+
+echo "Let's see who's logged into the system $(who)"
